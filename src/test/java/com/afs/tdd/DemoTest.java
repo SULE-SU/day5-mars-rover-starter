b6545c0ffbd0ed745ba80ba94be6b9b547489cbd
SULE <sule1@oocl.com>
2025-09-05 16:25:50 +0800

    test:update test should_return_coordinates_when_input_commend_M_direction_N

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -6,14 +6,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    public void should_return_y_plus_when_input_commend_M(){
+    public void should_return_coordinates_when_input_commend_M_direction_N(){
         String commands = "M";
 
         MarsRover marsRover = new MarsRover(0, 0,"N");
         marsRover.executeCommands(commands);
 
-        assertEquals(0, marsRover.getY());
-    }
+        assertEquals(0, marsRover.getX());
+        assertEquals(1, marsRover.getY());
+        assertEquals("N", marsRover.getDirection());
 
+    }
 
 }
