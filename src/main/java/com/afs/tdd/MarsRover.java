3cbb45af2fdc85d0f085d0cde34e4a74fa8b709c
SULE <sule1@oocl.com>
2025-09-05 15:29:51 +0800

    feat:add test should_return_y_plus_when_input_commend_M

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,17 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    private int x;
+    private int y;
+    private String direction;
+    public MarsRover(int x, int y, String direction) {
+    }
+
+
+    public int getY() {
+        return y;
+    }
+
+    public void executeCommands(String commands) {
+    }
+}
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,18 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    public void should_return_y_plus_when_input_commend_M(){
+        String commands = "M";
 
+        MarsRover marsRover = new MarsRover(0, 0,"N");
+        marsRover.executeCommands(commands);
+
+        assertEquals(0, marsRover.getY());
     }
+
+
 }
