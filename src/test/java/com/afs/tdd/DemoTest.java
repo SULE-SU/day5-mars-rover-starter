b025e62450f09150bd54a0ebf6a98a70e6fc4b3a
SULE <sule1@oocl.com>
2025-09-07 12:55:59 +0800

    test:add should_return_coordinates_when_input_command_B_direction_E unitTest

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -180,6 +180,19 @@ class DemoTest {
         assertEquals("S", marsRover.getDirection());
     }
 
+    @Test
+    public void should_return_coordinates_when_input_command_B_direction_E(){
+        String commands = "B";
+
+        MarsRover marsRover = new MarsRover(0, 0,"E");
+        marsRover.executeCommands(commands);
+
+        assertEquals(-1, marsRover.getX());
+        assertEquals(0, marsRover.getY());
+        assertEquals("E", marsRover.getDirection());
+    }
+
+
 
 
 }
